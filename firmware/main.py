7cd1e8b29f7049d1ed4f4dfb2a79e4025d9dba79
Muhammad Irtaza <irtazanaqvi05@gmail.com>
2025-07-12 17:17:02 +0500

    updated firmware and added shortcuts!

--- firmware/main.py
+++ firmware/main.py
@@ -2,39 +2,48 @@ import board
 import digitalio
 import neopixel
 import time
+import usb_hid
+
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
 
 # Setup
 NUM_LEDS = 8
 LED_PIN = board.GP3
 BRIGHTNESS = 0.3
 
-# LED strip setup
 leds = neopixel.NeoPixel(LED_PIN, NUM_LEDS, brightness=BRIGHTNESS, auto_write=False)
 
-# button pins
-button_pins = [board.GP0, board.GP1, board.GP2, board.GP4]
+# button pins and actions
+button_configs = [
+    {"pin": board.GP0, "keys": [Keycode.CONTROL, Keycode.C]},  # Copy
+    {"pin": board.GP1, "keys": [Keycode.CONTROL, Keycode.V]},  # Paste
+    {"pin": board.GP2, "keys": [Keycode.CONTROL, Keycode.Z]},  # Undo
+    {"pin": board.GP4, "keys": [Keycode.CONTROL, Keycode.Y]},  # Redo
+]
 
 # buttons setup
 buttons = []
-for pin in button_pins:
-    btn = digitalio.DigitalInOut(pin)
+for cfg in button_configs:
+    btn = digitalio.DigitalInOut(cfg["pin"])
     btn.direction = digitalio.Direction.INPUT
-    btn.pull = digitalio.Pull.UP  # buttons go LOW when pressed
+    btn.pull = digitalio.Pull.UP
     buttons.append(btn)
 
+# HID keyboard
+keyboard = Keyboard(usb_hid.devices)
 
 # LED animation functions!!
 def gradient_animation():
-    # Light up LEDs with a gradient (red to yellow to green)
     colors = [
-        (255, 0, 0),    # red
+        (255, 0, 0),
         (255, 64, 0),
         (255, 128, 0),
         (255, 192, 0),
         (255, 255, 0),
         (128, 255, 0),
         (64, 255, 0),
-        (0, 255, 0),    # green
+        (0, 255, 0),
     ]
     for i in range(NUM_LEDS):
         leds[i] = colors[i]
@@ -47,25 +56,26 @@ def set_all(color):
     leds.show()
 
 def press_animation():
-    # A quick left-to-right wipe
     for i in range(NUM_LEDS):
         leds[i] = (0, 128, 255)
         leds.show()
-        time.sleep(0.03)
-    time.sleep(0.05)
-    set_all((0, 255, 0))  # Back to green
+        time.sleep(0.02)
+    set_all((0, 255, 0))
 
-
-# Startup Animation
+# Startup Effect
 gradient_animation()
-time.sleep(0.3)
-set_all((0, 255, 0))  # green after boot
+time.sleep(0.2)
+set_all((0, 255, 0))
 
 # Main Loop
 while True:
-    for btn in buttons:
-        if not btn.value:  # Button pressed (LOW)
+    for i, btn in enumerate(buttons):
+        if not btn.value:  # Button pressed
             press_animation()
-            while not btn.value:  # wait until release
+            keycombo = button_configs[i]["keys"]
+            keyboard.press(*keycombo)
+            time.sleep(0.1)
+            keyboard.release_all()
+            while not btn.value:
                 time.sleep(0.01)
     time.sleep(0.01)
